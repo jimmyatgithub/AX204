adbd85bb768456cad4fbb6ca4474550f2f8c582f
Struan James Ruari Henstock <struanjamesruarihenstock@Jimmy-Henstocks-MackBookPRO.local>
2016-09-23 17:38:44 +0800

    myBlog

--- HTML5/draw.js
+++ HTML5/draw.js
@@ -9,3 +9,22 @@ ctx.fillRect(100,100,100,100);
 ctx.strokeStyle = "rgb(60, 100, 255)";
 ctx.strokeRect(80, 80, 140, 140);
 ctx.clearRect(120, 120, 60, 60);
+
+ctx4.fillStyle = 'rgb(0,100,100)';
+var x = 0;
+var y = 0;
+var width = 300;
+var height = 300;
+ctx4.fillRect(x, y, width, height);
+while (width > 0) {
+	x += 10;
+	y += 10;
+	width -= 20;
+	height -= 20;
+	ctx4.clearRect(x, y, width, height);
+	x += 10;
+	y += 10;
+	width -= 20;
+	height -= 20;
+	ctx4.fillRect(x, y, width, height);
+}
