cce97472c747aa9f9fd262a43d600ab3666e2a48
Struan James Ruari Henstock <struanjamesruarihenstock@Jimmy-Henstocks-MackBookPRO.local>
2016-12-09 17:01:45 +0800

    haha

--- PhaserGame/app.js
+++ PhaserGame/app.js
@@ -104,10 +104,10 @@ function update() {
 	// Keys are pressed, what hapens
 	if (cursors.left.isDown) {
 			player.body.velocity.x = -150;
-			player.animations.play('left');
-	} else if (cursors.right.isDown); {
+			player.animations.play('left')
+	} else if (cursors.right.isDown) {
 		player.body.velocity.x = 150;
-		player.animations.play('right');
+		player.animations.play('right')
 	} else {
 			player.animations.stop();
 			player.frame = 4;
